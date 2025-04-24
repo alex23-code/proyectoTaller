fd8ebe3b0b31eefeb42e1e97db39c749cd841c5a
FedericAlegre <121946483+FedericAlegre@users.noreply.github.com>
2025-04-23 23:58:23 -0300

    actualizacion de pagina principal

--- app/Views/Contenidos/Nosotros_view.php
+++ app/Views/Contenidos/Nosotros_view.php
@@ -1,23 +1,14 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset = "utf-8">
-        <meta name ="viewport" content = "width=device-width, initial-scale=1">
-        <title> BootStrap de p </title>
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity=" " crossorigin="">
-        <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""> </script>
-        <link href="assets/css/miEstilo2.css" rel="stylesheet" >
-    </head>
     <body>
+        <div class="content">
         <h1>Hola Mundo </h1>
         
         <p> Somos una empresa que se dedica a la venta de relojes de mesa y pared con 
         diseños únicos e innovadores. Utilizamos materia prima de calidad. Elaboramos 
         productos a la medida del cliente. </p>
+        </div>
     </body>
-    <footer>
-           
-    </footer>
 </html>
 
 
--- app/Views/Contenidos/Principal_view.php
+++ app/Views/Contenidos/Principal_view.php
@@ -35,15 +35,20 @@
                 </button>
             </div>
         </section>
+        <div>
+            <a href="<?php echo base_url('Comercializacion'); ?>">
+                <img style="margin: 10px 0px; width: 100%; text-align: left" src="assets/img/imagen1_principal.png" alt="">
+            </a>
+        </div>
         <!-- categorias y productos -->
-        <section class="Container" style="display: flex;">
+        <section class= "container-fluid" style="display: flex;">
             <div>
                 <div class="Category-title">
                     <h2>Categorías</h2>
                 </div>
                 <aside class="sidebar">
                     <ul>
-                        <li class="active"><a href="#">Hombres</a></li>
+                        <li><a href="#">Hombres</a></li>
                         <li><a href="#">Mujeres</a></li>
                         <li><a href="#">ACCESORIOS</a></li>
                         <li><a href="#">Outlet</a></li>
@@ -68,9 +73,9 @@
                 <!-- Producto 2 -->
                 <div class="col-md-3">
                     <div class="product-card">
-                        <img src="assets/img/RemeraHombre1.png" alt="Camisa Hammer Black">
+                        <img src="assets/img/RemeraHombre2.png" alt="...">
                         <p>$55.000</p>
-                        <h6>CAMISA HAMMER BLACK [PREVENTA 7/5]</h6>
+                        <h6>Camiseta adidas Selección Argentina Titular 24/25 De Hombre</h6>
                         <span>6 cuotas de $10.000 sin interés</span>
                         <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i></button>
                     </div>
@@ -79,9 +84,9 @@
                 <!-- Producto 3 -->
                 <div class="col-md-3">
                     <div class="product-card">
-                        <img src="assets/img/RemeraHombre1.png" alt="Kit Sport Corto Black">
+                        <img src="assets/img/RemeraHombre3.png" alt="...">
                         <p>$40.000</p>
-                        <h6>KIT SPORT CORTO BLACK [PREVENTA 21/4]</h6>
+                        <h6>Camiseta adidas Inter Miami Titular 22/23 Messi De Hombre</h6>
                         <span>6 cuotas de $6.666,67 sin interés</span>
                         <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i> </button>
                     </div>
@@ -90,11 +95,54 @@
                 <!-- Producto 4 -->
                 <div class="col-md-3">
                     <div class="product-card">
-                        <img src="assets/img/RemeraHombre1.png" alt="Catsuit Fierce Pink">
+                        <img src="assets/img/RemeraHombre4.png" alt="...">
                         <p>$50.000</p>
-                        <h6>CATSUIT FIERCE PINK</h6>
+                        <h6>Camiseta Puma Manchester City Pre-Match De Hombre</h6>
                         <span>6 cuotas de $8.333,33 sin interés</span>
                         <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i></button>
+                    </div>
+                </div>
+                <!-- siguiente fila -->
+                <div class="row">
+                    <!-- Producto 1 -->
+                    <div class="col-md-3">
+                        <div class="product-card">
+                            <img src="assets/img/RemeraHombre1.png" alt="...">
+                            <p>$60.000</p>
+                            <h6>Camiseta Musculosa River Plate 24/25 adidas Hombre</h6>
+                            <span>6 cuotas de $10.000 sin interés</span>
+                            <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i></button>
+                        </div>
+                    </div>
+                    <!-- Producto 2 -->
+                    <div class="col-md-3">
+                        <div class="product-card">
+                            <img src="assets/img/RemeraHombre2.png" alt="...">
+                            <p>$55.000</p>
+                            <h6>Camiseta adidas Selección Argentina Titular 24/25 De Hombre</h6>
+                            <span>6 cuotas de $10.000 sin interés</span>
+                            <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i></button>
+                        </div>
+                    </div>
+                    <!-- Producto 3 -->
+                    <div class="col-md-3">
+                        <div class="product-card">
+                            <img src="assets/img/RemeraHombre3.png" alt="...">
+                            <p>$40.000</p>
+                            <h6>Camiseta adidas Inter Miami Titular 22/23 Messi De Hombre</h6>
+                            <span>6 cuotas de $6.666,67 sin interés</span>
+                            <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i> </button>
+                        </div>
+                    </div>
+                    <!-- Producto 4 -->
+                    <div class="col-md-3">
+                        <div class="product-card">
+                            <img src="assets/img/RemeraHombre4.png" alt="...">
+                            <p>$50.000</p>
+                            <h6>Camiseta Puma Manchester City Pre-Match De Hombre</h6>
+                            <span>6 cuotas de $8.333,33 sin interés</span>
+                            <button class="btn btn-primary mt-0"><i class="fa-solid fa-cart-shopping"></i></button>
+                        </div>
                     </div>
                 </div>
             </div>
