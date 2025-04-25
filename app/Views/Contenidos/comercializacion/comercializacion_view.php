c1ad0430d088e1cd0713e029bb914f221fdb518f
Alex23 <alexaltamirano53@hotmail.com>
2025-04-25 19:32:35 -0300

    Avance de la página comercialización

--- app/Views/Contenidos/comercializacion/comercializacion_view.php
+++ app/Views/Contenidos/comercializacion/comercializacion_view.php
@@ -1,21 +1,76 @@
-<h1>Comercialización</h1>
+<h1 class="titulo">Comercializacion</h1>
+<img src="assets/img/portadaComercializacion.jpg" alt="" width="100%" height="50%">
+
+<link href="./assets/css/miEstiloComercializacion.css" rel="stylesheet">
 
   <section class="container-fluid">
       <aside class="sidebar">
-      <a href="#contenidopago" class="btn btn-primary mb-2">
+      <a href="#contenidopago" class="btn">
     
       Métodos de pago
       </a>
-      <a href="#contenidoenvios" class="btn btn-primary mb-2">
+      <a id="mostrarBoton2" href="#contenidoenvios" class="btn">
     
     Métodos de envío
     </a>
-    <a href="#preguntasfrecuentes" class="btn btn-primary mb-2">
+    <a id="mostrarboton3" href="#preguntas-frecuentes" class="btn">
     
     Preguntas Frecuentes
     </a>
-      </aside>
-    <main class="contenido">
-      asdasdasd
-    </main>
 </section>
+
+<section id="contenidopago" class="bloques">
+      <h1 >Tarjeta de crédito o debito</h1>
+      <div >
+      <i class="fa-brands fa-cc-visa fa-lg"></i>
+      <i class="fa-brands fa-cc-mastercard fa-lg"></i>
+      <p> 6 cuotas sin interes para tarjetas de créditos bancarizadas</p>
+      
+      <img src="assets/img/naranja.png" alt="" width="20" height="20">
+      <p>Tarjeta naranja plan z hasta 9 cuotas sin interes</p> 
+
+      <i class="fa-solid fa-credit-card"></i>
+      <p> Tarjetas de débito de todos los bancos</p>
+      <h1 >MercadoPago</h1>
+      <img src="assets/img/mercadopago.png" alt="" width="60" height="25">
+
+     <p>Pagando con dinero en cuenta obtenes 2% de descuentos los lunes</p>
+      <h1 > Efectivo</h1>
+       <i class="fa-solid fa-money-bill-wave fa-lg"></i>
+      <p>Tenemos distintas alternativas de pago en efectivo</p>
+      <img src="assets/img/rapipago.jpg" alt="" width="60" height="50">
+      <p>Rapipago</p>
+      <img src="assets/img/pagofacil.jpg" alt="" width="30" height="30">
+      <p>PagoFacil</p>
+      <h1>Transferencias</h1>
+      <i class="fa-solid fa-money-bill-transfer"></i>
+      <p>Pagando por transferencia bancaria 
+        obtenes un 10% de descuentos para compras mayores a $70.000 </p>
+      </div>
+      
+    
+      </section>
+
+      <section id="contenidoenvios" class="bloques">
+     <h1>Puedes recibir tu compra por:</h1>  
+      <p>Envio a domicilio</p>
+      <p>Zona Gran Buenos Aires: Hasta 1 dia hábil</p>
+      <p>Interior: De 7 a 10 dias hábiles</p>
+      <p>El costo dependera del codigo postal declarado</p>
+</section>
+
+      <section id="preguntas-frecuentes" class="bloques">
+      <h1>Preguntas Frecuentes:</h1>  
+      <p>¿Cómo realizar un cambio?</p>
+      <p>Si necesitas realizar un cambio o devolución de uno o más productos, podes acceder a la seccion cambios y devoluciones para solicitar la devolucion.
+        Tienes de 10 dias para solicitar la devolucion <p/>
+      <p>¿Cómo sé que mi pago fue acreditado?</p>
+      <p>Una vez que tu pago haya sido acreditado correctamente por administración, recibirás un correo electrónico informándote el número de acreditación.</p>
+      <p>¿Cuántos cambios gratis puedo realizar?</p>
+      <p>Uno, a menos que el motivo del cambio sea un error nuestro de despacho. Los cambios siguientes tienen un costo de envío de $8.990</p>
+    <p>Me arrepentí de comprar mi producto. ¿Lo puedo devolver?</p>
+    <p>Sí. Todo lo que compres en https://www.sporting.com.ar lo podes cambiar a menos que se trate de productos identificados como LIQUIDACIÓN cuyos cambios estarán condicionados a disponibilidad de stock. 
+      En caso de querer cambiar por otro producto, podrás hacerlo eligiendo cualquier artículo de los que estén en la web. Si el nuevo producto elegido tiene un costo mayor, se te cobrará la diferencia correspondiente ( ver: https:///politicas-de-cambio).</p>
+    
+    
+    </section>
